6a39d99836b08bc6502281ad4e4f8e73ece10e86
ganinw13120 <gan131201@gmail.com>
2021-12-11 23:04:57 +0700

    path with subpath

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 type DrawerProps = {
-    lineWidth : number
-    lineCap : CanvasLineCap
-    strokeStyle : string
+    lineWidth: number
+    lineCap: CanvasLineCap
+    strokeStyle: string
 }
 
 type Position = {
-    x : number
-    y : number
+    x: number
+    y: number
 }
 
 type Path = {
-    startPosition : Position
-    stopPosition : Position
-    isFocus : boolean
+    startPosition: Position
+    stopPosition: Position
+    isFocus: boolean
 }
 
-const Drawer: React.FC<DrawerProps> = ({lineWidth, lineCap, strokeStyle}) => {
+const Drawer: React.FC<DrawerProps> = ({ lineWidth, lineCap, strokeStyle }) => {
     const containerRef = useRef<HTMLDivElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -27,11 +27,11 @@ const Drawer: React.FC<DrawerProps> = ({lineWidth, lineCap, strokeStyle}) => {
 
     const [paths, setPaths] = useState<Array<Path>>([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!containerRef || !containerRef.current) return
-       const resizeObserver = new ResizeObserver(() => {resizeCanvas()});
-       resizeObserver.observe(containerRef.current)
-       return (): void => { resizeObserver.unobserve(containerRef.current!) };
+        const resizeObserver = new ResizeObserver(() => { resizeCanvas() });
+        resizeObserver.observe(containerRef.current)
+        return (): void => { resizeObserver.unobserve(containerRef.current!) };
     }, [containerRef])
 
     const resizeCanvas = () => {
@@ -41,7 +41,7 @@ const Drawer: React.FC<DrawerProps> = ({lineWidth, lineCap, strokeStyle}) => {
         canvas.width = containerRef.current!.clientWidth;
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!canvasRef || !containerRef) return;
 
         const canvas = canvasRef.current
@@ -51,7 +51,7 @@ const Drawer: React.FC<DrawerProps> = ({lineWidth, lineCap, strokeStyle}) => {
         canvas.width = containerRef.current!.clientWidth;
 
         const context = canvas.getContext('2d')
-        
+
         context!.lineWidth = lineWidth;
         context!.lineCap = lineCap;
         context!.strokeStyle = strokeStyle;
@@ -60,14 +60,15 @@ const Drawer: React.FC<DrawerProps> = ({lineWidth, lineCap, strokeStyle}) => {
         setCanvasContext(context);
     }, [canvasRef, containerRef]);
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!canvasContext) return;
-        canvasContext?.clearRect(0,0,canvasRef.current!.clientWidth,canvasRef.current!.clientHeight);
-        paths.forEach(e=>{
+        canvasContext?.clearRect(0, 0, canvasRef.current!.clientWidth, canvasRef.current!.clientHeight);
+        paths.forEach(e => {
             canvasContext.moveTo(e.startPosition.x, e.startPosition.y);
             canvasContext.beginPath();
-            canvasContext.lineTo(e.startPosition.x, e.startPosition.y);
-            canvasContext.lineTo(e.stopPosition.x, e.stopPosition.y);
+            generateSubPath(e).forEach(subPath => {
+                canvasContext.lineTo(subPath.x, subPath.y);
+            })
             canvasContext.stroke();
         })
         canvasContext!.lineWidth = lineWidth;
@@ -75,55 +76,68 @@ const Drawer: React.FC<DrawerProps> = ({lineWidth, lineCap, strokeStyle}) => {
         canvasContext!.strokeStyle = strokeStyle;
     }, [paths])
 
-    const handleMouseMove = (e : React.MouseEvent<HTMLDivElement>) => {
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
         if (isMouseClick && canvasContext) {
-            const position : Position = {
-                x : e.pageX,
-                y : e.pageY,
+            const position: Position = {
+                x: e.pageX,
+                y: e.pageY,
             }
             setPaths((p) => {
                 const temp = [...p];
-                temp.find(e=>e.isFocus)!.stopPosition = position;
+                temp.find(e => e.isFocus)!.stopPosition = position;
                 return temp
             })
         }
     }
 
-    const handleMouseDown = (e : React.MouseEvent<HTMLDivElement>) => {
-        const position : Position = {
-            x : e.pageX,
-            y : e.pageY,
+    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+        const position: Position = {
+            x: e.pageX,
+            y: e.pageY,
         }
         setPaths((p) => {
             const temp = [...p];
-            temp.push({startPosition : position, stopPosition : position, isFocus : true})
+            temp.push({ startPosition: position, stopPosition: position, isFocus: true })
             return temp
         })
         setMouseClick(true);
     }
-    const handleMouseUp = (e : React.MouseEvent<HTMLDivElement>) => {
-        const position : Position = {
-            x : e.pageX,
-            y : e.pageY,
+    const handleMouseUp = (e: React.MouseEvent<HTMLDivElement>) => {
+        const position: Position = {
+            x: e.pageX,
+            y: e.pageY,
         }
         setPaths((p) => {
             const temp = [...p];
-            if (!temp.find(e=>e.isFocus)) return temp;
-            temp.find(e=>e.isFocus)!.stopPosition = position;
-            temp.find(e=>e.isFocus)!.isFocus = false;
+            if (!temp.find(e => e.isFocus)) return temp;
+            temp.find(e => e.isFocus)!.stopPosition = position;
+            temp.find(e => e.isFocus)!.isFocus = false;
             return temp
         })
         setMouseClick(false);
     }
 
+    const generateSubPath = (path: Path): Position[] => {
+        const middleX = (path.startPosition.x + path.stopPosition.x) / 2
+        const middleStartPosition: Position = {
+            x: middleX,
+            y: path.startPosition.y
+        }
+        const middleStopPosition: Position = {
+            x: middleX,
+            y: path.stopPosition.y
+        }
+        return [path.startPosition, middleStartPosition, middleStopPosition, path.stopPosition];
+    }
+
     const onClear = () => {
-        canvasContext?.clearRect(0,0,canvasRef.current!.clientWidth,canvasRef.current!.clientHeight);
+        canvasContext?.clearRect(0, 0, canvasRef.current!.clientWidth, canvasRef.current!.clientHeight);
         setPaths([]);
     }
 
     return (
         <>
-            <button onClick={onClear} style={{position: 'absolute'}}>Clear</button>
+            <button onClick={onClear} style={{ position: 'absolute' }}>Clear</button>
             <div ref={containerRef} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp} onMouseMove={handleMouseMove} className='canvas-container'>
                 <canvas ref={canvasRef} />
             </div>
