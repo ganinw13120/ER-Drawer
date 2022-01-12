f7c1ae0718740825e1a60e58c173ff7933d7d70e
ganinw13120 <gan131201@gmail.com>
2022-01-12 16:30:53 +0700

    feat: dynamic line path

--- src/utils/generateShortestPath.ts
+++ src/utils/generateShortestPath.ts
@@ -11,18 +11,75 @@ const generateShortestPath = (param: LinePathParameters): Position[] => {
     const path: Array<Array<Position>> = [];
     if (startPoint && stopPoint) {
         if (
-            (startPos.x < stopPos.x && startPoint.position === PointPosition.Right && stopPoint.position === PointPosition.Left) ||
-            (startPos.x > stopPos.x && startPoint.position === PointPosition.Left && stopPoint.position === PointPosition.Right)
+            (startPos.x + lineStartTickDistance < stopPos.x - lineStartTickDistance && startPoint.position === PointPosition.Right && stopPoint.position === PointPosition.Left) ||
+            (startPos.x - lineStartTickDistance > stopPos.x + lineStartTickDistance && startPoint.position === PointPosition.Left && stopPoint.position === PointPosition.Right)
         ) {
             path.push(getSimplePath(param));
         } else {
-            path.push(
-                getRoundTop(param),
-                getRoundBottom(param)
-            )
+            if (startPoint.position === stopPoint.position) {
+                if (startPoint.position === PointPosition.Right) {
+
+                    if (startPos.x > stopPos.x) {
+                        path.push(
+                            getRoundTopRight({
+                                startPos: stopPos,
+                                stopPos: startPos,
+                                startPoint: stopPoint,
+                                stopPoint: startPoint
+                            }),
+                            getRoundBottomRight({
+                                startPos: stopPos,
+                                stopPos: startPos,
+                                startPoint: stopPoint,
+                                stopPoint: startPoint
+                            }),
+                        )
+                    }
+                    else {
+                        path.push(
+                            getRoundTopRight(param),
+                            getRoundBottomRight(param)
+                        )
+                    }
+                } else {
+                    console.log('Case 0')
+                    if (startPos.x > stopPos.x) {
+                        console.log('Case 1')
+                        path.push(
+                            getRoundBottomLeft(param),
+                        )
+                    }
+                    else {
+                        console.log('Case 2')
+                        path.push(
+                            getRoundBottomLeft(param)
+                        )
+                    }
+                }
+            }
+            else if (startPoint.position !== stopPoint.position) {
+                if (startPoint.position === PointPosition.Left) {
+                    path.push(
+                        getRountUp(param),
+                    )
+                } else {
+                    path.push(
+                        getRountUp({
+                            startPos: stopPos,
+                            stopPos: startPos,
+                            startPoint: stopPoint,
+                            stopPoint: startPoint
+                        }).reverse(),
+                    )
+                }
+            }
         }
     } else {
         path.push(getSimplePath(param));
+    }
+
+    if (path.length === 0) {
+        path.push(getSimplePath(param)) /// Prevent Error (Test purpose)
     }
 
     // if ((startPos.x < stopPos.x && startPoint?.position))
@@ -73,11 +130,45 @@ const calDistance = (pos: Position[]): number => {
     return totalDistance;
 }
 
-const getRoundTop = (param: LinePathParameters): Position[] => {
+const getRoundBottomLeft = (param: LinePathParameters): Position[] => {
     const {
         startPos,
         stopPos,
         startPoint,
+        stopPoint,
+    } = param;
+    const stopBox = stopPoint?.box;
+    const startBox = startPoint?.box;
+    let checkPoints: Position[] = [];
+    const boxRect = stopBox?.ref.current?.getClientRects()[0];
+    const startBoxRect = startBox?.ref.current?.getClientRects()[0];
+    /// 1.
+    checkPoints.push({ x: startPos.x, y: startPos.y });
+    if (startPos.x + lineStartTickDistance >= boxRect!.x - pathReservX) {
+        /// 2.
+        checkPoints.push({ x: startPos.x - pathReservX, y: startPos.y });
+        /// 3.
+        checkPoints.push({ x: startPos.x - pathReservX, y: startBoxRect!.y + startBoxRect!.height });
+    }
+    else {
+        /// 2.
+        checkPoints.push({ x: startPos.x - pathReservX, y: startPos.y });
+        /// 3.
+        checkPoints.push({ x: startPos.x - pathReservX, y: (boxRect!.y + boxRect!.height) + pathReservY });
+        /// 4.
+        checkPoints.push({ x: stopPos.x - pathReservX, y: (boxRect!.y + boxRect!.height) + pathReservY });
+    }
+    /// 5.
+    checkPoints.push({ x: stopPos.x - pathReservX, y: stopPos.y });
+    /// 6.
+    checkPoints.push({ x: stopPos.x, y: stopPos.y });
+    return checkPoints;
+}
+
+const getRoundTopRight = (param: LinePathParameters): Position[] => {
+    const {
+        startPos,
+        stopPos,
         stopPoint,
     } = param;
     let checkPoints: Position[] = [];
@@ -104,11 +195,10 @@ const getRoundTop = (param: LinePathParameters): Position[] => {
     return checkPoints;
 }
 
-const getRoundBottom = (param: LinePathParameters): Position[] => {
+const getRoundBottomRight = (param: LinePathParameters): Position[] => {
     const {
         startPos,
         stopPos,
-        startPoint,
         stopPoint,
     } = param;
     const stopBox = stopPoint?.box;
@@ -132,6 +222,49 @@ const getRoundBottom = (param: LinePathParameters): Position[] => {
     checkPoints.push({ x: stopPos.x + pathReservX, y: stopPos.y });
     /// 6.
     checkPoints.push({ x: stopPos.x, y: stopPos.y });
+    return checkPoints;
+}
+
+/// Oposite Position Case
+const getRountUp = (param: LinePathParameters): Position[] => {
+    const {
+        startPos,
+        stopPos,
+        startPoint,
+        stopPoint,
+    } = param;
+    const startBox = startPoint?.box;
+    const startBoxRect = startBox?.ref.current?.getClientRects()[0];
+    let checkPoints: Position[] = [];
+
+    /// 1.
+    checkPoints.push({ x: startPos.x, y: startPos.y });
+
+    /// 2.
+    checkPoints.push({ x: startPos.x - pathReservX, y: startPos.y });
+
+    if (startPos.y < stopPos.y) {
+        /// 3.
+        checkPoints.push({ x: startPos.x - pathReservX, y: startBoxRect!.y + startBoxRect!.height + pathReservY })
+
+        /// 4.
+        checkPoints.push({ x: stopPos.x + pathReservX, y: startBoxRect!.y + startBoxRect!.height + pathReservY })
+    }
+    else {
+        /// 3.
+        checkPoints.push({ x: startPos.x - pathReservX, y: startBoxRect!.y - pathReservY })
+
+        /// 4.
+        checkPoints.push({ x: stopPos.x + pathReservX, y: startBoxRect!.y - pathReservY })
+
+    }
+
+    /// 5.
+    checkPoints.push({ x: stopPos.x + pathReservX, y: stopPos.y })
+
+    /// 6.
+    checkPoints.push({ x: stopPos.x, y: stopPos.y })
+
     return checkPoints;
 }
 
